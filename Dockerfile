3886b2f668a5622a42632bf9922d31c1a26b3d43
Max Pershin <mepershin@gmail.com>
2023-04-16 13:26:40 +0300

    remove devenv

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM alpine:3.17.3
-
-RUN apk update && \
-    apk add neovim
-
-WORKDIR /root
-RUN mkdir -p ~/.config/nvim
--- devenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-image_name=nvim-nocopy-paste-devenv
-
-docker build -t $image_name .
-docker run -it \
-    -v $PWD/lua:/root/.config/nvim/lua:ro \
-    -v $PWD/plugin:/root/.config/nvim/plugin:ro \
-    $image_name /bin/sh
