e66b1313075e55fcebe60d082e0a23a2636266a7
Max Pershin <mepershin@gmail.com>
2024-01-19 11:22:13 +0200

    Update nocopy-paste.lua

--- plugin/nocopy-paste.lua
+++ plugin/nocopy-paste.lua
@@ -1,7 +1,7 @@
 require("nocopy-paste")
 
-vim.api.nvim_set_keymap('v', '<C-v>', "<cmd>lua _nocopy_paste()<CR>",
-                        { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('v', '<C-v>', "<cmd>lua _nocopy_paste()<CR>",
+--                        { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('v', 'p', "<cmd>lua _nocopy_paste()<CR>",
                         { noremap = true, silent = true })
